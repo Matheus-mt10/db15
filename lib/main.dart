55d473fb3697751996fe2ce518f6e27a2479de20
matheusDev <matheus.info29@gmail.com>
2024-03-14 15:17:25 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     title: "Meu primeiro App",
-    home: PaginaPrincipal(),
+    home: TelaListaDeCompras(),
   ));
 }
 
@@ -23,18 +23,60 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
             centerTitle: true,
             titleTextStyle: const TextStyle(color: Colors.white),
             backgroundColor: Colors.black),
-        body: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.account_circle),
-                hintText: "Digite o seu login",
+        body: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.account_circle),
+                  hintText: "Digite o seu login",
+                ),
               ),
-            ),
-            Text("Matheus é o melhor professor"),
-            Text("Matheus é o melhor professor"),
-            Text("Matheus é o melhor professor"),
-          ],
+              TextFormField(
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.password), hintText: "Digite a sua senha"),
+                obscureText: true,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text("Login")),
+              )
+            ],
+          ),
         ));
+  }
+}
+
+class TelaListaDeCompras extends StatefulWidget {
+  const TelaListaDeCompras({super.key});
+
+  @override
+  State<TelaListaDeCompras> createState() => _TelaListaDeComprasState();
+}
+
+class _TelaListaDeComprasState extends State<TelaListaDeCompras> {
+
+   List <String> listaDeProdutos = [
+      "SB Dunk Low Court Purple",
+      "Air Max 1 '86 OG Summit White",
+      "SB Dunk Low City Of Love"
+    ];
+    
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Lista de compras"),
+        ),
+        body: ListView.builder(
+            itemCount: listaDeProdutos.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(listaDeProdutos[index]),
+              );
+            }));
   }
 }
